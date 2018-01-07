94f2d459c71b37bfbe14a18f362fe81bc6bcc7dd
nicolepilsworth <nicole_pilsworth@hotmail.com>
2018-01-07 15:56:49 +0000

    Add parameter details to graph

--- graph.py
+++ graph.py
@@ -3,13 +3,32 @@ import plotly.graph_objs as go
 import numpy as np
 
 class Graph:
-    def __init__(self, tSteps, y1s, y2s):
+    def __init__(self, tSteps, y1s, y2s, learnType, epsilon, gamma, alpha, nGames):
         self.line1 = np.array(y1s)
         self.line2 = np.array(y2s)
         self.xarray = np.array(tSteps)
+        self.learnType = learnType
+        self.layout = go.Layout(
+            title='Random learning curve vs. RL curve: ' + learnType + '\n Epsilon: ' + str(epsilon) + ', Gamma: ' + str(gamma) + ', Alpha: ' + str(alpha) + '\n Number of Games: ' + str(nGames),
+            xaxis=dict(
+                title='Number of games played',
+                titlefont=dict(
+                    size=18,
+                    color='#7f7f7f'
+                )
+            ),
+            yaxis=dict(
+                title='Average score per game',
+                titlefont=dict(
+                    size=18,
+                    color='#7f7f7f'
+                )
+            )
+        )
 
     def plot(self):
         py.tools.set_credentials_file(username='nicolepilsworth', api_key='RIr45yea9BGg46iuh29e')
+
 
         # Code used from https://plot.ly/python/line-charts/#new-to-plotly
         trace1 = go.Scatter(
@@ -19,12 +38,13 @@ class Graph:
         )
 
         trace2 = go.Scatter(
-            name="Q-learning",
+            name=self.learnType,
             x = self.xarray,
             y = self.line2
         )
 
         data = [trace1, trace2]
+        fig = go.Figure(data=data, layout=self.layout)
 
-        py.plotly.plot(data, filename='basic-line')
+        py.plotly.plot(fig, filename='basic-line')
         print("Lines plotted")
