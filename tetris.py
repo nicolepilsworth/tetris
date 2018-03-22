3f9a24c21c9e632a5af71204fc66e4ed35b54491
nicolepilsworth <nicole_pilsworth@hotmail.com>
2018-03-22 16:16:42 +0000

    set to column number of large Tetris board

--- tetris.py
+++ tetris.py
@@ -84,8 +84,8 @@ def main():
   # Universal variables
   nGames = 20000
   tSteps = [100*i for i in range(1, int(nGames/100 + 1))]
-  nRows = 8
-  nCols = 6
+  nRows = 4
+  nCols = 10
   maxPerEpisode = 500
   boardSize = str(nRows) + " rows * " + str(nCols) + " cols"
 
